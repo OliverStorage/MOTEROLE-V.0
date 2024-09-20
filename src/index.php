29bfe9600bba1e26b0b4a4973e84dc31b5e6cf48
VerM-OMM <omolina.k11832651@umak.edu.ph>
2024-09-20 23:51:53 +0800

    first commit

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moterole</title>
+    <link href="output.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- title -->
+    <div
+        class="bg-[url('./images/mainbg.png')] bg-cover bg-center h-screen w-screen flex flex-col justify-center items-center space-y-[100px]">
+        <span class="text-7xl text-white -mt-[100px] font-kavoon text-center font-outline-4">Little Letter
+            <br>Adventure</span>
+        <!-- btns -->
+        <div class="space-x-20 text-white font-kavoon text-5xl">
+            <a href="" class="bg-[#0E3532]  outline-[#C78B36] outline  outline-8 rounded-[50px] px-20 py-4">PLAY</a>
+            <a href="" class="bg-[#0E3532]  outline-[#C78B36] outline  outline-8 rounded-[50px] px-20 py-4">EXIT</a>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
