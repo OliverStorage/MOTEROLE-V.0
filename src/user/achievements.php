4b261b7322819baa3b4e268d79d0759e87d4a77d
VerM-OMM <omolina.k11832651@umak.edu.ph>
2024-09-24 09:02:08 +0800

    asdasasd

---
+++ src/user/achievements.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moterole</title>
+    <link href="../output.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="bg-[url('./images/mainbg.png')] bg-cover bg-center h-screen w-screen p-5 relative">
+        <div class="bg-[#D68E5E] bg-center bg-cover h-full rounded-3xl outline outline-[5px] outline-[#F2D43F] p-1 grid grid-cols-5">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+    </div>
+</body>
+
+</html>
