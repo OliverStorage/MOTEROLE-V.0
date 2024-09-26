f1c4377a0a60a4e60f7db07b4d8c56f475efd7c5
VerM-OMM <omolina.k11832651@umak.edu.ph>
2024-09-27 02:09:49 +0800

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       fontFamily: {
         rubikbubbles: ["Rubik Bubbles","serif"],
         rubikspray: ["Rubik Spray Paint","system-ui"],
+        nunito: ["Nunito","san-serif"],
       }
     },
   },
